615825060869586b231b622168c6b7b93d5d3c96
sanju <sanju123@gmail.com>
2024-02-28 11:48:38 +0530

    change2

--- one.java
+++ one.java
@@ -2,7 +2,8 @@ public class hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("welcome to java")
+		System.out.println("welcome to java");
+		System.out.println("welcome to spt");
 	}
 }
 
